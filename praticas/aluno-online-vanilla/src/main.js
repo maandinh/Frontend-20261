391fb86ed738da0e6d8d3710cf5e831ff3a42ed2
Amanda Soares <amanda.soares2@iesb.edu.br>
2026-03-23 10:43:17 -0300

    Pratica 02 incompleta

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,35 @@
-import './style.css'
+import "./style.css";
+
+const email = document.querySelector("#email");
+const senha = document.querySelector("#senha");
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function (e) {e.preventDefault();
+  const emailErro = document.querySelector("#emailErro");
+  const senhaErro = document.querySelector("#senhaErro");
+
+  let temErro = false;
+
+  emailErro.textContent = '';
+  senhaErro.textContent = '';
+
+  if (email.value == "") {
+    emailErro.textContent = "O campo de email é obrigatório.";
+    temErro = true;
+  } else if (!email.value.includes("@") || !email.value.includes(".")) {
+    emailErro.textContent = "Email inválido";
+    temErro = true;
+  }
+
+  if (senha.value == "") {
+    senhaErro.textContent = "O campo senha é obrigatório.";
+    temErro = true;
+  } else if (senha.value.length < 8) {
+    senhaErro.textContent = "Senha deve ter no mínimo 8 caracteres";
+    temErro = true;
+  }
+
+  if (temErro) return;
+
+  console.log("Formulário válido");
+});
