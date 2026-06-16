ed98af7c13a2545356bd88c21af9caeb78a49ebb
Amanda Soares <amanda.soares2@iesb.edu.br>
2026-06-16 13:12:37 -0300

    feat: conclui pratica06. Fecha #10

--- praticas/aluno-online-react/src/pages/Requerimentos.jsx
+++ praticas/aluno-online-react/src/pages/Requerimentos.jsx
@@ -1,11 +1,9 @@
 import Main from "../components/Main";
-import Sidebar from "../components/Sidebar";
 import Tabela from "../components/Tabela";
 
 function Requerimentos() {
     return (
         <div className="flex h-screen bg-gray-50">
-            <Sidebar />
             <Main 
                 titulo="Meus Requerimentos" 
                 subtitulo="Faça solicitações online para a Secretaria"
